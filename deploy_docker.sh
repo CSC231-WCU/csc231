89371a7c91466e1b58848cce6a53a9a03eced6a8
Linh Ngo <lngo@wcupa.edu>
2020-09-11 10:50:46 -0400

    passwordless SSH taz

--- deploy_docker.sh
+++ deploy_docker.sh
@@ -3,5 +3,5 @@ echo $1
 rm -Rf _site/*
 echo ${PWD}
 rm -Rf _site
-docker run -v ${PWD}:/srv/jekyll -it jekyll/jekyll:stable /srv/jekyll/deploy.sh --verbose
-scp -r _site/* $1:~/public_html/csc231/
+docker run -v ${PWD}:/srv/jekyll  -p 127.0.0.1:4000:4000/tcp jekyll/jekyll:stable /srv/jekyll/deploy.sh --verbose
+
--- site.sh
+++ site.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -x
 
+bundle install
 bundle exec jekyll build
 scp -rq _site/* lngo@cs.wcupa.edu:~/public_html/csc231
