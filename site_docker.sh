7bad7fc05a9f55112a845da333f5088240e30a00
Linh B. Ngo <lngo@wcupa.edu>
2022-07-18 12:50:23 -0400

    initial settings for new layout

--- site_docker.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-rm -Rf _site/*
-docker run -v ${PWD}:/srv/jekyll -it linhbngo/jekyll:latest bundle exec jekyll build
-rsync -a _site/ lngo@cs.wcupa.edu:~/public_html/csc231/
-#scp -r _site/* lngo@cs.wcupa.edu:~/public_html/csc231/
